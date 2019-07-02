c4fcb1c934941f46ea0f1e42f9d75a55aed72340
preet <preet.sidhu@bounceshare.com>
2019-07-02 17:40:48 +0530

    config entities

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,13 @@
 import * as urls from './urls'
+import fonts from './fonts'
+import colors from './colors'
+import spacing from './spacing'
+import { Strings } from './strings'
 
 export {
   urls,
-  
+  fonts,
+  colors,
+  spacing,
+  Strings,
 }
---
+++ src/config/spacing.js
@@ -0,0 +1,16 @@
+const unit = 2
+
+// Todo add px / em
+
+const spacing = {
+  xxsmall: unit * 1, //2
+  xsmall: unit * 2, //4
+  small: unit * 3, //6
+  default: unit * 4, //8
+  medium: unit * 6, //12
+  large: unit * 8, //16
+  xlarge: unit * 12, //24
+  xxlarge: unit * 16, //32
+}
+
+export default spacing
