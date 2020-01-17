722bf21eab38357d6b4c8f38ded3282f36e0ebcb
Shihab US <shihabudheen@bounceshare.com>
2020-01-17 16:33:24 +0530

    intersection hook added

--- src/components/interscetionObserver.js
+++ src/components/interscetionObserver.js
@@ -1,6 +1,27 @@
 import React, { useEffect, useRef } from "react";
 import axios from "axios";
 
+function useIsOnScreenHook(ref, option = {}) {
+  const [isIntersecting, setIntersecting] = React.useState(false);
+  const observerInstance = new IntersectionObserver(([entry]) => {
+    setIntersecting(entry.isIntersecting);
+  }, option);
+  useEffect(() => {
+    if (ref.current) {
+      if (ref.current) {
+        observerInstance.observe(ref.current);
+      }
+    }
+    return () => {
+      if (ref.current) {
+        observerInstance.unobserve(ref.current);
+      }
+    };
+  }, [observerInstance, option, ref]);
+
+  return isIntersecting;
+}
+
 export default function IO() {
   const [loading, setLoading] = React.useState(false);
   const [dataState, setData] = React.useState([]);
@@ -9,37 +30,44 @@ export default function IO() {
   const parentDiv = useRef(null);
   const EOL = useRef(null);
 
-  const getUsers = (p = 0) => {
-    setLoading(true);
-    axios
-      .get(`https://reqres.in/api/users?page=${p + 1}&per_page=5`)
-      .then(res => {
-        const {
-          data: { page, total_pages, data }
-        } = res;
-        dataState.push(...data);
-        setData(dataState);
-        setPage(page);
-        setIsMore(pageState + 1 !== total_pages);
-        setLoading(false);
-      });
-  };
+  const test = useIsOnScreenHook(EOL, { root: parentDiv.current });
 
-  const Observer = new IntersectionObserver(
-    ([entry]) => {
-      if (entry.isIntersecting) {
-        getUsers(pageState);
-      }
-    },
-    {
-      root: parentDiv.current
-    }
-  );
   useEffect(() => {
-    if (EOL.current) {
-      Observer.observe(EOL.current);
+    const getUsers = (p = 0) => {
+      setLoading(true);
+      axios
+        .get(`https://reqres.in/api/users?page=${p + 1}&per_page=5`)
+        .then(res => {
+          const {
+            data: { page, total_pages, data }
+          } = res;
+          dataState.push(...data);
+          setData(dataState);
+          setPage(page);
+          setIsMore(pageState + 1 !== total_pages);
+          setLoading(false);
+        });
+    };
+    if (test) {
+      getUsers(pageState);
     }
-  }, [EOL, Observer]);
+  }, [dataState, pageState, test]);
+
+  // const Observer = new IntersectionObserver(
+  //   ([entry]) => {
+  //     if (entry.isIntersecting) {
+  //       getUsers(pageState);
+  //     }
+  //   },
+  //   {
+  //     root: parentDiv.current
+  //   }
+  // );
+  // useEffect(() => {
+  //   if (EOL.current) {
+  //     Observer.observe(EOL.current);
+  //   }
+  // }, [EOL, Observer]);
 
   return (
     <div>
